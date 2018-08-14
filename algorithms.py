3fb082f5d12ed61fc7c9898829de202c46a35fbe
ChernovAndrey <chernov.andrey.98@mail.ru>
2018-08-15 00:38:52 +0300

    add log regres

--- algorithms.py
+++ algorithms.py
@@ -12,9 +12,11 @@ import numpy as np
 from myUtils import readData
 from create_datasets import LS, QS, path_save
 import matplotlib.pyplot as plt
-begin_lr = 0.0002 # begin learning rate
-epochs = 5000
+import math
+begin_lr = 0.01 # begin learning rate
+epochs = 10000
 count_features = 2
+eps = 1e-10
 # функция стоимтости SSE
 def adaline_step(x_train,y_train,w,lr): 
     
@@ -30,6 +32,25 @@ def adaline_step(x_train,y_train,w,lr):
     cost = cost/2.0
     return cost,new_w
 
+
+def sigmoid(x):
+    return 1/(1+math.exp(-x))
+
+def log_regres_step(x_train,y_train,w,lr): 
+    
+    # прямой проход и обратный проход делается по сути одновременно, но старые веса не меняются во время прохода
+    cost = 0
+    new_w = w.copy()
+    for i,el in enumerate(x_train): 
+        z = w[0] + w[1]*el[0] + w[2]*el[1]
+        cost += -( y_train[i]*math.log(sigmoid(z)+eps) - (1-y_train[i])*math.log(1-sigmoid(z)+eps) )
+        new_w[0] -= lr*( (y_train[i] - sigmoid(z)) * (-1) ) 
+        new_w[1] -= lr*( (y_train[i] - sigmoid(z)) * (-el[0]) ) 
+        new_w[2] -= lr*( (y_train[i] - sigmoid(z)) * (-el[1]) ) 
+    cost = cost/2.0
+    return cost,new_w
+
+
 def merge_and_mix_train_data(data0,data1,label0,label1):
     data = np.concatenate( (data0,data1), axis = 0)
     label = np.concatenate( (label0,label1), axis = 0)
@@ -40,21 +61,22 @@ def train(x_train,y_train):
     w = np.random.uniform(-1,1,count_features + 1)
     lr = begin_lr
     for i in range(epochs):
-        cost,w= adaline_step(x_train,y_train,w,lr)
+        cost,w= log_regres_step(x_train,y_train,w,lr)
+#        cost,w= adaline_step(x_train,y_train,w,lr)
         if (i+1)%100 == 0:
             print('step = ', i, '; cost function = ', cost)
-        if i == 200:
-            lr = 5e-5
-        if i == 9000:
-            lr = 1e-8
+#        if i == 200:
+#            lr = 5e-5
+#        if i == 9000:
+#            lr = 1e-8
     return w
 
 def evaluate(x_test,y_test,w):
     count_error = 0
     for i, el in enumerate(x_test):
         z = w[0] + w[1]*el[0] + w[2]*el[1]
-        y_pred = -1
-        if z >= 0.0:
+        y_pred = 0
+        if z >= 0.5:
             y_pred = 1
         if y_test[i] != y_pred:
             print('value pred error = ',z)
@@ -66,10 +88,11 @@ def evaluate(x_test,y_test,w):
 def show_results(data0,data1,w):
     plt.scatter(data0[:,0], data0[:,1], color= 'red', marker='o')
     plt.scatter(data1[:,0], data1[:,1], color='blue' , marker= 'x')
-    x1_line_point = [-5,5] # для линии раздленеия
+    x1_line_point = [-7,0,7] # для линии раздленеия
     x2_line_point = x1_line_point
     x2_line_point[0] = (-w[0]-w[1]*x1_line_point[0])/w[2]
     x2_line_point[1] = (-w[0]-w[1]*x1_line_point[1])/w[2]
+    x2_line_point[2] = (-w[0]-w[1]*x1_line_point[2])/w[2]
     print(x2_line_point)
     plt.plot(x1_line_point, x2_line_point)
 #    plt.plot([-10,10], [-25.23456990138067, 22.200020415768552])
@@ -80,7 +103,7 @@ def show_results(data0,data1,w):
 if __name__ == "__main__":
     data0 = readData(os.path.join(path_save, LS),'data0')
     data1 = readData(os.path.join(path_save, LS),'data1')
-    label0 = np.full(len(data0),-1)
+    label0 = np.full(len(data0),0) # -1 для adaline, у остальных - 0
     label1 = np.full(len(data0),1)
     data, label = merge_and_mix_train_data(data0,data1,label0,label1)        
     count_train = len(data)//2
@@ -88,4 +111,5 @@ if __name__ == "__main__":
     w = train(x_train,y_train)    
     evaluate(x_test,y_test,w)
     evaluate(data,label,w)
-    show_results(data0,data1,w)    
+    show_results(data0,data1,w)
+#%%
